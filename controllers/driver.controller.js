2189043af2be90a2b9d2089b14dac39c7eb0fda5
jh-menguito <jh.menguito@gmail.com>
2024-06-09 19:50:56 +0800

    backend

---
+++ controllers/driver.controller.js
@@ -0,0 +1,22 @@
+const db = require("../models");
+const Driver = db.drivers;
+
+//create and save new driver
+exports.create = (req, res) => {
+
+};
+
+//retrieve all drivers from db
+exports.findAll = (req, res) => {
+
+};
+
+//update a driver
+exports.update = (req, res) => {
+
+};
+
+//delete a driver
+exports.delete = (req, res) => {
+
+};
